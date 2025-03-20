02acedabc377d136ae2880ec0db6745ff1d176f0
NyanPanDev <nekonyansoft@proton.me>
2025-03-20 20:50:36 +0000

    Adding basic files and understanding how components work

--- lib/main/app.ts
+++ lib/main/app.ts
@@ -13,8 +13,8 @@ export function createAppWindow(): void {
     frame: false,
     titleBarStyle: 'hiddenInset',
     title: 'Kanna',
-    maximizable: false,
-    resizable: false,
+    maximizable: true,
+    resizable: true,
     webPreferences: {
       preload: join(__dirname, '../preload/preload.js'),
       sandbox: false,
